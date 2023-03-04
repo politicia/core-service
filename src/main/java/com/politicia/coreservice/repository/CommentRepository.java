52698583d83a4c4007b597beea577efbadc9ae30
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2023-03-05 02:02:56 +0900

    added required methods

--- src/main/java/com/politicia/coreservice/repository/CommentRepository.java
+++ src/main/java/com/politicia/coreservice/repository/CommentRepository.java
@@ -1,7 +1,13 @@
 package com.politicia.coreservice.repository;
 
 import com.politicia.coreservice.domain.Comment;
+import com.politicia.coreservice.domain.Post;
+import com.politicia.coreservice.domain.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByPost(Post post);
+
+    Page<Comment> findByUser(User user);
 }
--- src/main/java/com/politicia/coreservice/repository/MediaRepository.java
+++ src/main/java/com/politicia/coreservice/repository/MediaRepository.java
@@ -1,7 +1,11 @@
 package com.politicia.coreservice.repository;
 
 import com.politicia.coreservice.domain.Media;
+import com.politicia.coreservice.domain.Post;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MediaRepository extends JpaRepository<Media, Long> {
+
+    Page<Media> findByPost(Post post);
 }
