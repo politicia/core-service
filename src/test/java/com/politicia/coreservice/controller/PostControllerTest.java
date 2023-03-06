4d2e26df5d53a60c5aff1636a57a59473d33ac95
Yany Choi <51287461+YanyChoi@users.noreply.github.com>
2023-03-06 11:35:16 +0900

    controller test code

---
+++ src/test/java/com/politicia/coreservice/controller/PostControllerTest.java
@@ -0,0 +1,185 @@
+package com.politicia.coreservice.controller;
+
+import com.politicia.coreservice.domain.User;
+import com.politicia.coreservice.dto.request.PostRequestDto;
+import com.politicia.coreservice.dto.response.PostResponseDto;
+import com.politicia.coreservice.service.PostService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(PostController.class)
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+
+    @Test
+    void createPost() throws Exception {
+        //given
+        User user = User.builder()
+                .id(1L)
+                .name("test")
+                .nationality("country")
+                .profilePic("https://profile.pic")
+                .build();
+        PostRequestDto requestDto = PostRequestDto.builder()
+                .userId(user.getId())
+                .title("title")
+                .text("text")
+                .build();
+
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post("/post")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content("{\n" +
+                                "    \"userId\": \"1\",\n" +
+                                "    \"title\": \"text\",\n" +
+                                "    \"text\": \"text\"\n" +
+                                "}"
+                        ))
+                .andExpect(status().isCreated());
+        verify(postService, times(1)).createPost(any(PostRequestDto.class));
+    }
+
+    @Test
+    void editPost() throws Exception {
+        //given
+        User user = User.builder()
+                .id(1L)
+                .name("test")
+                .nationality("country")
+                .profilePic("https://profile.pic")
+                .build();
+        PostRequestDto requestDto = PostRequestDto.builder()
+                .userId(user.getId())
+                .title("title")
+                .text("text")
+                .build();
+        PostResponseDto postResponseDto = PostResponseDto.builder()
+                .postId(1L)
+                .user(user)
+                .title("titleEdited")
+                .text("textEdited")
+                .build();
+
+        //when
+        mockMvc.perform(put("/post/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content("{\n" +
+                        "    \"userId\": \"1\",\n" +
+                        "    \"title\": \"titleEdited\",\n" +
+                        "    \"text\": \"textEdited\"\n" +
+                        "}"
+                ))
+                .andExpect(status().isOk());
+        verify(postService, times(1)).editPost(any(Long.class), any(PostRequestDto.class));
+
+    }
+
+    @Test
+    void deletePost() throws Exception {
+        //given
+        Long postId = 1L;
+        //when
+
+        //then
+        mockMvc.perform(delete("/post/1"))
+                .andExpect(status().isOk());
+        verify(postService, times(1)).deletePost(postId);
+    }
+
+    @Test
+    void getPost() throws Exception {
+        //given
+        PostResponseDto expectedDto = PostResponseDto.builder()
+                .postId(1L)
+                .title("title")
+                .build();
+        //when
+        when(postService.getPostById(1L)).thenReturn(expectedDto);
+        //then
+        mockMvc.perform(get("/post/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.postId").value(expectedDto.getPostId()))
+                .andExpect(jsonPath("$.title").value(expectedDto.getTitle()));
+    }
+
+    @Test
+    void getPostListByDate() throws Exception {
+        //given
+        LocalDate date = LocalDate.now();
+        PostResponseDto postA = PostResponseDto.builder()
+                .postId(1L)
+                .title("title")
+                .build();
+        PostResponseDto postB = PostResponseDto.builder()
+                .postId(2L)
+                .title("title")
+                .build();
+        Page<PostResponseDto> posts = new PageImpl<>(List.of(postA, postB));
+        //when
+        when(postService.getPostsByDate(date, 0)).thenReturn(posts);
+        //then
+        mockMvc.perform(get("/post/list?date=2023-03-05"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.postId").value(postA.getPostId()))
+                .andExpect(jsonPath("$.title").value(postA.getTitle()));
+    }
+
+    @Test
+    void getPostListByUser() throws Exception {
+        //given
+        User user = User.builder()
+                .id(1L)
+                .name("test")
+                .build();
+        PostResponseDto postA = PostResponseDto.builder()
+                .postId(1L)
+                .user(user)
+                .title("title")
+                .build();
+        PostResponseDto postB = PostResponseDto.builder()
+                .postId(2L)
+                .user(user)
+                .title("title")
+                .build();
+        Page<PostResponseDto> posts = new PageImpl<>(List.of(postA, postB));
+
+        //when
+        when(postService.getPostsByUser(1L, 0)).thenReturn(posts);
+
+        //then
+        mockMvc.perform(get("/post/list?userId=1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.").value(postA.getPostId()))
+                .andExpect(jsonPath("$.title").value(postA.getTitle()));
+
+    }
+}
---
+++ src/test/java/com/politicia/coreservice/controller/UserControllerTest.java
@@ -0,0 +1,87 @@
+package com.politicia.coreservice.controller;
+
+import com.politicia.coreservice.domain.User;
+import com.politicia.coreservice.dto.request.UserRequestDto;
+import com.politicia.coreservice.dto.response.UserResponseDto;
+import com.politicia.coreservice.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    void getUser() throws Exception {
+        //given
+        User user = User.builder()
+                .id(1L)
+                .name("test")
+                .profilePic("https://profile.pic")
+                .nationality("testCountry")
+                .build();
+
+        //when
+        when(userService.getUser(1L)).thenReturn(user.toDto());
+        //then
+
+        mockMvc.perform(get("/user/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(user.getId()))
+                .andExpect(jsonPath("$.name").value(user.getName()))
+                .andExpect(jsonPath("$.profilePic").value(user.getProfilePic()))
+                .andExpect(jsonPath("$.nationality").value(user.getNationality()));
+
+    }
+
+    @Test
+    void createUser() throws Exception {
+        //given
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                .name("test")
+                .profilePic("https://profile.pic")
+                .nationality("testCountry")
+                .build();
+        UserResponseDto expectedUser = UserResponseDto.builder()
+                .id(1L)
+                .name("test")
+                .profilePic("https://profile.pic")
+                .nationality("testCountry")
+                .build();
+
+        //when
+        when(userService.createUser(userRequestDto)).thenReturn(expectedUser);
+        //then
+        mockMvc.perform(post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content("{\n" +
+                                "    \"name\": \"test\",\n" +
+                                "    \"nationality\": \"testCountry\",\n" +
+                                "    \"profilePic\": \"https://profile.pic\"\n" +
+                                "}"
+                        ))
+                .andExpect(status().isCreated());
+
+    }
+}
